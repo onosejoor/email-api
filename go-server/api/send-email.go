7e6499956768cf24107e67414631d5eb021bd9a3
Onos Ejoor <156698969+onosejoor@users.noreply.github.com>
2025-09-25 23:02:05 +0100

    update code

--- go-server/api/send-email.go
+++ go-server/api/send-email.go
@@ -2,9 +2,20 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"sync"
 
 	"github.com/onosejoor/email-api/mailer"
+	gomail "gopkg.in/mail.v2"
+)
+
+var (
+	dialer *gomail.Dialer
+	sender gomail.SendCloser
+	once   sync.Once
 )
 
 type EmailRequest struct {
@@ -48,4 +59,26 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	writeJSON(w, http.StatusOK, JsonResponse{true, "Email sent successfully"})
+}
+
+func initDialer() {
+	dialer = gomail.NewDialer("smtp.gmail.com", 465, os.Getenv("GMAIL_USER"), os.Getenv("GMAIL_APP_PASSWORD"))
+	var err error
+	sender, err = dialer.Dial()
+	if err != nil {
+		log.Fatalf("Failed to dial SMTP: %v", err)
+	}
+}
+
+// SendEmail sends using a persistent SMTP connection
+func SendEmail(from, to, subject, body string) error {
+	once.Do(initDialer)
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", fmt.Sprintf("%s <%s>", from, os.Getenv("GMAIL_USER")))
+	m.SetHeader("To", to)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", body)
+
+	return gomail.Send(sender, m)
 }
